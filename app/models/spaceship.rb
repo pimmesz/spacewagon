bbbe438a38a6bb1114c7907630c9b37b63c259ff
Rick te Molder <rtminnovations@gmail.com>
2018-02-26 18:03:09 +0100

    spaceship validations

---
+++ app/models/spaceship.rb
@@ -0,0 +1,6 @@
+class Spaceship < ApplicationRecord
+  belongs_to :user
+
+  validates :location, :speed, :capacity, :captain, :price_per_day, :user_id, presence: true
+  validates :speed, :capacity, :price_per_day, numericality: true
+end
