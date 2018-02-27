2c8634a8a38db2445e175c34db550fd1092fb302
Rick te Molder <rtminnovations@gmail.com>
2018-02-27 14:35:22 +0100

    added has_many bookings to spacehip

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,5 +1,6 @@
 class Spaceship < ApplicationRecord
   belongs_to :user
+  has_many :bookings
 
   validates :name, :location, :speed, :capacity, :captain, :price_per_day, :user_id, presence: true
   validates :speed, :capacity, :price_per_day, numericality: true
