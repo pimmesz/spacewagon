f0917e54f7d2cf45cc807712f9e3cc01903cb7f3
Pim Zonneveld <pimzonneveld@gmail.com>
2018-02-27 11:22:30 +0100

    Added controller and view bookings. Added it to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+
+  get 'bookings/new'
+
+  get 'bookings/show'
+
   devise_for :users
   root to: 'pages#home'
+  resources :bookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
