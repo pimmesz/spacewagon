6f0005157bac2974e3e4203535aec2f5ecf772ee
Pim Zonneveld <pimzonneveld@gmail.com>
2018-02-27 11:18:24 +0100

    Added controller and views for spaceship. Added it to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  # get 'spaceships/index'
+
+  # get 'spaceships/new'
+
+  # get 'spaceships/show'
+
   devise_for :users
   root to: 'pages#home'
+  resources :spaceships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
