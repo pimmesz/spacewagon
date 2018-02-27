595b678fbfd519fdb7b5eed41d832c6c69b9665d
Pim Zonneveld <pimzonneveld@gmail.com>
2018-02-27 11:11:23 +0100

    Added controler and view of users. Created resources users in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+
+  # get 'users/new'
+
+  # get 'users/show'
+
   devise_for :users
   root to: 'pages#home'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
