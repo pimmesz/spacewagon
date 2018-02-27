18fa3777f3ef973cdfbe396b854ca9e9d7a73526
Pim Zonneveld <pimzonneveld@gmail.com>
2018-02-27 18:22:45 +0100

    Fixed issue with permits in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,22 +10,15 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-  private
-
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
   def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
+  # For additional fields in app/views/devise/registrations/new.html.erb
 
-  protected
+   # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name], :account_update, keys: [:username], :first_name, :last_name, address_attributes: [:country, :state, :city, :area, :postal_code]])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, address_attributes: [:country, :state, :city, :area, :postal_code]])
 
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name], :account_update, keys: [:username], :first_name, :last_name, address_attributes: [:country, :state, :city, :area, :postal_code]])
-#    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-#    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-#    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, address_attributes: [:country, :state, :city, :area, :postal_code]])
-
-    
   end
 end
