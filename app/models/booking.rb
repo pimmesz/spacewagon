14b404dde7fcded9689b33430644abef47e3b6d1
Steef <stefanodegenkamp@gmail.com>
2018-02-28 15:43:13 +0100

    Logo upgrade and end date before start date bugfix

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,15 @@ belongs_to :spaceship
 belongs_to :user
 validates :start_date, presence: true
 validates :end_date, presence: true
+validate :date_validation
 # validates :spaceship, uniqueness: { scope: :user }
+
+def date_validation
+  if self[:end_date] < self[:start_date]
+    errors[:end_date] << "Start date must be before end date"
+    return false
+  else
+    return true
+  end
+end
 end
