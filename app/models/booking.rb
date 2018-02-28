420b2a2dca119d23cc282d2a423982ef3ee22b88
Steef <stefanodegenkamp@gmail.com>
2018-02-28 16:48:00 +0100

    Book a spaceship in the past bugfix

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ belongs_to :user
 validates :start_date, presence: true
 validates :end_date, presence: true
 validate :date_validation
+validate :date_validation_past
 # validates :spaceship, uniqueness: { scope: :user }
 
 def date_validation
@@ -12,6 +13,15 @@ def date_validation
     return false
   else
     return true
+  end
+end
+
+def date_validation_past
+  if self[:start_date] >= DateTime.now.to_date
+    true
+  else
+    errors[:start_date] << "can't be in the past"
+    return false
   end
 end
 end
