b6b0cd944fcd536028e136761a737fd230f75d4d
Pim Zonneveld <pimzonneveld@gmail.com>
2018-03-01 19:46:59 +0100

    Add javascript for smooth scrolling links

--- app/assets/javascripts/components/smoothScrollingLinks.js
+++ app/assets/javascripts/components/smoothScrollingLinks.js
@@ -5,7 +5,7 @@
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 1000, function(){
         window.location.hash = hash;
       });
     }
